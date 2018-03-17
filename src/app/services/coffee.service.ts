783b08cfb1a28d6aa8a2b1e2bb9bc3cc84fca4b4
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-03-17 13:57:36 +0800

    delay

--- src/app/services/coffee.service.ts
+++ src/app/services/coffee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as i from '../state/app.interfaces';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class CoffeeService {
@@ -8,7 +9,9 @@ export class CoffeeService {
   constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get<i.Coffee[]>('assets/list.json');
+    return this.http.get<i.Coffee[]>('assets/list.json').pipe(
+      delay(1500)
+    );
   }
 
 }
