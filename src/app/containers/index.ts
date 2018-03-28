a6b898ecacdc2cf1b1f4bda73b7ece5ce9fef1b6
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-03-28 09:55:03 +0800

    rename file

--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -1,4 +1,3 @@
 export * from './cart-page/cart-page.component';
-export * from './cart2-page/cart2-page.component';
 export * from './list-page/list-page.component';
 export * from './customize-page/customize-page.component';
