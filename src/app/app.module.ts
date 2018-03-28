7471eae72cbcd2f6060c4f94cd27f577a7c2c135
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-03-28 23:22:48 +0800

    store freeze in dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,27 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
+
 import { environment } from '../environments/environment';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoffeeService } from './services/coffee.service';
+
 import { appReducer } from './state/app.reducer';
 import { appInitialState } from './state/app.init';
 import { AppEffects } from './state/app.effects';
-import { CoffeeService } from './services/coffee.service';
-import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
 import { ListPageComponent, CartPageComponent } from './containers';
 import { HeaderComponent, PayComponent } from './components';
 import { SharedModule } from './modules/shared';
+import { App, AppState } from './state/app.interfaces';
+
+const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
 
 const CONTAINERS = [ListPageComponent, CartPageComponent];
 const COMPONENTS = [HeaderComponent, PayComponent];
@@ -35,7 +41,8 @@ const COMPONENTS = [HeaderComponent, PayComponent];
     StoreModule.forRoot(
       { app: appReducer },
       {
-        initialState: { app: appInitialState }
+        initialState: { app: appInitialState },
+        metaReducers
       }),
     EffectsModule.forRoot([AppEffects]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
